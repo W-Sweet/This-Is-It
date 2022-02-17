4280563d2a6cf7a8151f8ad607ec8ab2842bd06a
W-Sweet <84293098+W-Sweet@users.noreply.github.com>
2022-02-16 22:49:29 -0500

    are we there yet

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -2,6 +2,16 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
+
+
+
+
+
+// I'm testing out how to use github, while watching magic the gathering ! 
+
+
+
+
 package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
